aa17a72ef97a15121e1674b575e29a6f4bdb9315
Ernesto <atomms@gmail.com>
2022-02-11 09:21:21 +0100

    computed field

--- odoo14-custom-addons/lista_tareas/models/models.py
+++ odoo14-custom-addons/lista_tareas/models/models.py
@@ -14,11 +14,11 @@ class lista_tareas(models.Model):
      descripcion = fields.Text()
 #
      @api.depends('prioridad')
-     def _value_pc(self):
+     def _value_urgente(self):
 
           for record in self:
-            #Si la prioridad es mayor que 10, se considera urgente, en otro caso no lo es
-            if record.prioridad>7:
+            #Si la prioridad es mayor que 8, se considera urgente, en otro caso no lo es
+            if record.prioridad>8:
                 record.urgente = True
             else:
                 record.urgente = False
